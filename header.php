a3b54ee23d163d275d814567dc924c97570cb6f2
Gaurav Kumar <arcanenilesh@gmail.com>
2023-10-26 09:49:26 +0530

    PHP conversion and created components

---
+++ header.php
@@ -0,0 +1,70 @@
+<header>
+    <div class="topnav py-2 relative z-50">
+        <div class="container flex justify-between flex-col md:flex-row">
+            <div class="phone-email mb-4 md:mb-0 col-span-4 flex md:block">
+                <a href="tel:7340740007" class="mr-4 md:mr-8 inline-block">
+                    <i class="fa-solid fa-phone mr-2 text-primary"></i>7340740007
+                </a>
+                <a class="inline-block" href="mailto:hr@baselineitdevelopment.com">
+                    <i class="fa-solid fa-envelope mr-2 text-primary"></i>hr@baselineitdevelopment.com
+                </a>
+            </div>
+            <div class="icons flex items-center">
+                <a href="https://www.instagram.com/baselineitdevelopmemt/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://in.linkedin.com/company/baselineitdevelopment"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://twitter.com/BaselineITDev/"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.facebook.com/BaselineItDevelopment.pvt.ltd"><i class="fa-brands fa-facebook"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="header sticky top-0 z-50 left-0 right-0 bg-transparent">
+        <div class="hidden shadow-md"></div>
+        <div class="container navbar py-4">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </label>
+                    <ul tabindex="0"
+                        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 label-md">
+                        <li><a>About</a></li>
+                        <li>
+                            <a>Services</a>
+                            <ul class="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Career</a></li>
+                        <li><a>Blog</a></li>
+                    </ul>
+                </div>
+                <a href="/" class="h-full inline-block"><img class="h-10" src="assets/logos/baselinelogo.svg" alt=""
+                        srcset=""></a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-1 label-md gap-4">
+                    <li><a>About</a></li>
+                    <li tabindex="0">
+                        <details>
+                            <summary>Services</summary>
+                            <ul class="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Career</a></li>
+                    <li><a>Blog</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn btn-primary">Contact Us</a>
+            </div>
+        </div>
+    </div>
+</header>
