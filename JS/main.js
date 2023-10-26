65f4725a3a698617e91abd5d96bf2ba2dee2bc5b
Nilesh Jaswal <arcanenilesh@gmail.com>
2023-10-26 09:56:21 +0530

    Revert "Created components "

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     /* Sticky Nav */
     $(window).scroll(function () {
-        var height = $('.header.sticky').height();
+        var height = $('header.sticky').height();
         var scrollTop = $(window).scrollTop();
 
         if (scrollTop >= height) {
-            $('.header.sticky').addClass('solid-nav-anim');
-            $('.header.sticky').addClass('shadow-md');
+            $('header.sticky').addClass('solid-nav-anim');
+            $('header.sticky').addClass('shadow-md');
         } else {
-            $('.header.sticky').removeClass('solid-nav-anim');
-            $('.header.sticky').removeClass('shadow-md');
+            $('header.sticky').removeClass('solid-nav-anim');
+            $('header.sticky').removeClass('shadow-md');
         }
     });
 
