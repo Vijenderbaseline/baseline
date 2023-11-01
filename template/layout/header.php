2ee009887fc249061d270c35f85c248b09b9965e
Vijenderbaseline <vijendersvijender@gmail.com>
2023-11-01 11:16:33 +0530

    Favicon files

--- template/layout/header.php
+++ template/layout/header.php
@@ -1,92 +1,153 @@
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="upgrade-insecure-requests"
+    />
+    <title>Web Development & Web Designing Company in Mohali, India</title>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-  <title>Web Development & Web Designing Company in Mohali, India</title>
-  <link rel="stylesheet" href="dist/output.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.2/swiper-bundle.min.css"
-    integrity="sha512-PRY5WanOn66pQYvwsRB8RtbxSr9FbDaieE2fRrBOOcjluzKggxMbmCiSMTL7Lw2m/oAdpalMYhxpz5LwO3Q4Ag=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-    rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Favicon code -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png" />
+    <link rel="manifest" href="assets/favicon/site.webmanifest" />
+    <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
 
-  <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
-</head>
 
-<body>
-<header class="topnav py-2 relative z-50">
-    <div class="container flex justify-between flex-col md:flex-row">
+    <link rel="stylesheet" href="dist/output.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.2/swiper-bundle.min.css"
+      integrity="sha512-PRY5WanOn66pQYvwsRB8RtbxSr9FbDaieE2fRrBOOcjluzKggxMbmCiSMTL7Lw2m/oAdpalMYhxpz5LwO3Q4Ag=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <script
+      src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
+      type="module"
+    ></script>
+  </head>
+
+  <body>
+    <header class="topnav py-2 relative z-50">
+      <div class="container flex justify-between flex-col md:flex-row">
         <div class="phone-email mb-4 md:mb-0 col-span-4 flex md:block">
-            <a href="tel:7340740007" class="mr-4 md:mr-8 inline-block font-semibold">
-                <i class="fa-solid fa-phone mr-2 text-primary"></i>7340740007
-            </a>
-            <a class="inline-block font-semibold" href="mailto:hr@baselineitdevelopment.com">
-                <i class="fa-solid fa-envelope mr-2 text-primary"></i>hr@baselineitdevelopment.com
-            </a>
+          <a
+            href="tel:7340740007"
+            class="mr-4 md:mr-8 inline-block font-semibold"
+          >
+            <i class="fa-solid fa-phone mr-2 text-primary"></i>7340740007
+          </a>
+          <a
+            class="inline-block font-semibold"
+            href="mailto:hr@baselineitdevelopment.com"
+          >
+            <i class="fa-solid fa-envelope mr-2 text-primary"></i
+            >hr@baselineitdevelopment.com
+          </a>
         </div>
         <div class="icons flex items-center">
-            <a href="https://www.instagram.com/baselineitdevelopmemt/"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://in.linkedin.com/company/baselineitdevelopment"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="https://twitter.com/BaselineITDev/"><i class="fa-brands fa-twitter"></i></a>
-            <a href="https://www.facebook.com/BaselineItDevelopment.pvt.ltd"><i class="fa-brands fa-facebook"></i></a>
+          <a href="https://www.instagram.com/baselineitdevelopmemt/"
+            ><i class="fa-brands fa-instagram"></i
+          ></a>
+          <a href="https://in.linkedin.com/company/baselineitdevelopment"
+            ><i class="fa-brands fa-linkedin"></i
+          ></a>
+          <a href="https://twitter.com/BaselineITDev/"
+            ><i class="fa-brands fa-twitter"></i
+          ></a>
+          <a href="https://www.facebook.com/BaselineItDevelopment.pvt.ltd"
+            ><i class="fa-brands fa-facebook"></i
+          ></a>
         </div>
-    </div>
-</header>
-<header class="header sticky top-0 z-50 left-0 right-0 bg-transparent">
-    <div class="hidden shadow-md"></div>
-    <div class="container navbar py-4">
+      </div>
+    </header>
+    <header class="header sticky top-0 z-50 left-0 right-0 bg-transparent">
+      <div class="hidden shadow-md"></div>
+      <div class="container navbar py-4">
         <div class="navbar-start">
-            <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </label>
-                <ul tabindex="0"
-                    class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 label-md">
-                    <li><a href="about-us.php">About</a></li>
-                    <li>
-                        <a href="services.php">Services</a>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Career</a></li>
-                    <li><a href="blog.php">Blog</a></li>
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 label-md"
+            >
+              <li><a href="about-us.php">About</a></li>
+              <li>
+                <a href="services.php">Services</a>
+                <ul class="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
                 </ul>
-            </div>
-            <a href="/" class="h-full inline-block"><img class="h-10" src="assets/logos/baselinelogo.svg" alt=""
-                    srcset=""></a>
-        </div>
-        <div class="navbar-center hidden lg:flex bg-secondary rounded-full shadow">
-            <ul class="menu menu-horizontal px-4 label-md gap-4">
-                <li><a href="about-us.php">About</a></li>
-                <li tabindex="0">
-                    <details>
-                        <summary><a href="services.php">Services</a></summary>
-                        <ul class="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </details>
-                </li>
-                <li><a>Career</a></li>
-                <li><a href="blog.php">Blog</a></li>
+              </li>
+              <li><a>Career</a></li>
+              <li><a href="blog.php">Blog</a></li>
             </ul>
+          </div>
+          <a href="/" class="h-full inline-block"
+            ><img
+              class="h-10"
+              src="assets/logos/baselinelogo.png"
+              alt=""
+              srcset=""
+          /></a>
+        </div>
+        <div
+          class="navbar-center hidden lg:flex bg-secondary rounded-full shadow"
+        >
+          <ul class="menu menu-horizontal px-4 label-md gap-4">
+            <li><a href="about-us.php">About</a></li>
+            <li tabindex="0">
+              <details>
+                <summary><a href="services.php">Services</a></summary>
+                <ul class="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </details>
+            </li>
+            <li><a>Career</a></li>
+            <li><a href="blog.php">Blog</a></li>
+          </ul>
         </div>
         <div class="navbar-end">
-            <a class="btn btn-primary rounded-full px-6 shadow">Contact Us</a>
+          <a class="btn btn-primary rounded-full px-6 shadow">Contact Us</a>
         </div>
-    </div>
-</header>
+      </div>
+    </header>
+  </body>
+</html>
